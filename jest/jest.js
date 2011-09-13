4a69eebcd611a3a1a9d5b804e51b53ac9cb95680
Ben Brooks Scholz <bbscholz@gmail.com>
2011-09-13 12:00:09 -0400

    added browser detection, improved structure, faster html generation

--- jest/jest.js
+++ jest/jest.js
@@ -1,6 +1,14 @@
 // jest is a unit testing framework.
 // 2011 (c) Ben Brooks Scholz. MIT Licensed.
 
+(function (window) {
+
+jest = function (moduleName) {
+	previousModule = jest.currentModule;
+    currentModule = moduleName;
+    results = [];
+};
+
 var Jest = {
     yes : function (testFunction, message) {
         // cast to boolean
@@ -15,47 +23,89 @@ var Jest = {
     no : function (testFunction, message) {
         // cast to boolean
         var result = !testFunction;
-        var testResult = {
+        var result = {
             boolResult : result,
             msgResult : message
         };
-        results.push(testResult);
+        results.push(result);
     },
     
     alike : function (testFunction, expected, message) {
         
     },
     
-    same : function ( testFunction, expected, message) {
+    unlike : function (testFunction, expected, message) {
+        
+    },
+    
+    same : function (testFunction, expected, message) {
+        
+    },
+    
+    different : function (testFunction, expected, message) {
         
     }
-
 };
 
-jest = function (moduleName) {
-	previousModule = jest.currentModule;
-    currentModule = moduleName;
-    results = [];
+// extend an object (extendee) with the properties of another object (extender)
+var extend = function (extendee, extender) {
+    var property;
+    for (property in extender) {
+        extendee[property] = extender[property];
+    }
+    return extendee;
 };
+
+var getBrowserString = function () {
+    var browser,
+        version = navigator.userAgent.split(' '),
+        parseVersion = function (position) {
+            return version[version.length-position].split('/')[1];
+        };
+    
+        
+    if (navigator.vendor === 'Apple Computer, Inc.') {
+        version = parseVersion(2);
+        browser = 'Safari';
+    } else if (navigator.vendor === 'Google Inc.') {
+        version = parseVersion(2);
+        browser = 'Chrome';
+    } else if (window.opera) {
+        version = parseVersion(1);
+        browser = 'Opera';
+    } else if (navigator.product === 'Gecko') {
+        version = parseVersion(1);
+        browser = 'Firefox';
+    }
+    console.log(browser + ' ' + version);
+    return browser + ' ' + version;
+};
+
+extend(window, Jest);
+
+extend(Jest, {
+    parseStats : function (currentTests) {
+        statsHtml = [];
+        statsHtml.push("<h1>Jest Test Suite</h1><div id=\"stat-summary\"><p id=\"user-info\">" + 
+                        getBrowserString() + "</p>");
+        statsHtml.push("<p id=\"passed-number\">6 out 6 tests passed.</p><p id=\"test-time\">execution time: 86 milliseconds.</p></div><h2 id=\"module-name\">" + currentModule + "</h2>");
+        statsHtml.push("<h2 id=\"test-group\">" + 
+                        currentTests +
+                        "</h2>");
+        return statsHtml.join('');
+    }
+});
 
 test = function (testsName, expectedTests, tests) {
     var currentTests = testsName;
     var expectedTests = expectedTests;
-    
-    yes = Jest.yes;
-    no = Jest.no;
 
     tests();
-
-    var htmlOutput = '<h1>Jest Test Suite</h1><h2>' + currentModule + '</h2>' +
-                     '<h3>' + currentTests + '</h3>';
-    
-    for (var i = 0; i < results.length; i++) {
-        htmlOutput += '<p>' + results[i].boolResult.toString() + '</p>';
-        htmlOutput += '<p>' + results[i].msgResult + '</p>';
-    }
-
-    document.body.innerHTML = htmlOutput;
+    this.document.body.innerHTML = Jest.parseStats(currentTests);
 };
+
+
+}(this));
+
 
 
