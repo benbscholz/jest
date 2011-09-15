a9dbce2a3bfce5f005ab3d64b28633896bdb874c
Ben Brooks Scholz <bbscholz@gmail.com>
2011-09-14 22:09:34 -0400

    same & different tests

--- examples/tests.js
+++ examples/tests.js
@@ -1,17 +1,35 @@
-jest('test');
+jest('jest tests');
 
-test('basic tests', 6, function() {
-    yes( true, 'true is true' );
-    yes( !false, 'not false is true' );
-    yes( !!true, 'not not true is true' );
-    yes( 1, 'truthy is true' );
+test('basic examples', 6, function() {
+    yes( true, 'yes: true is true' );
+    yes( !false, 'yes: not false is true' );
+    yes( !!true, 'yes: not not true is true' );
+    yes( 1, 'yes: truthy is true' );
     
-    no( false, 'false is false' );
-    no( !true, 'not true is false' );
-    no( !!false, 'not not false is false' ); 
-    no( 0, 'falsy is false' ); 
+    no( false, 'no: false is false' );
+    no( !true, 'no: not true is false' );
+    no( !!false, 'no: not not false is false' ); 
+    no( 0, 'no: falsy is false' ); 
     
-    //alike();
+    alike(1, 1, 'alike: one is one');
+    alike(true, true, 'alike: true is true');
+    alike('jest', 'jest', 'alike: string is string');
+    var a = [1]
+    alike(a, a, 'alike: array is array');
     
-    //same();
+    unlike(1, 2, 'unlike: one is not two');
+    unlike(true, false, 'unlike: true is not false');
+    unlike('jest', 'jester', 'unlike: string is not string');
+    unlike([1], [2], 'unlike: array is not array');
+    
+    same(1, 1, 'same: one is one');
+    same('jest', 'jest', 'same: string is string');
+    same([1], [1], 'same: array is array');
+    same({ 1 : 1 }, { 1 : 1 }, 'same: object is object');
+    
+    different(1, 2, 'different: one is not two');
+    different('jest', 'jester', 'different: string is not string');
+    different([1], [0], 'different: array is not array');
+    different({ 1 : 0 }, { 1 : 1}, 'different: objects are different')
 });
+
