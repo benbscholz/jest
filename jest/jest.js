6ec188c04894547a6e827ec745357e0dd6bed1bd
Ben Brooks Scholz <bbscholz@gmail.com>
2011-10-01 19:56:16 -0500

    added range function simplified jestStats data

--- jest/jest.js
+++ jest/jest.js
@@ -3,6 +3,46 @@
 
 (function (window) {
 
+"use strict";
+    
+var Module = function (name) {
+    this.name = name;
+    this.tests = [];
+};
+
+Module.prototype = {
+    setup : function () {
+        var jestTests = id('jest-tests'),
+            module = id(this.name.split(' ').join('-')),
+            moduleHeader;
+        
+        if (!module) {
+            module = create('ol');
+            module.id = this.name.split(' ').join('-');
+            moduleHeader = create('h2');
+            moduleHeader.innerHTML = this.name;
+            module.appendChild(moduleHeader);
+            jestTests.appendChild(module);
+        } 
+    },
+    
+    run : function() {
+        var test;
+        
+        while (this.tests.length) {
+            test = this.tests.shift();
+            this.currentTest = test;
+            test.setup.call(test);
+            test.run.call(test);
+            test.finish.call(test);
+        }
+    },
+    
+    finish : function () {
+        return;
+    }
+};
+
 var Test = function (module, name, expected, callback) {
     this.module = module;
     this.name = name;
@@ -15,19 +55,10 @@ var Test = function (module, name, expected, callback) {
 
 Test.prototype = {
     setup : function () {
-        var jestTests = byId('jest-tests'),
-            module = byId(this.module.split(' ').join('-')),
+        var jestTests = id('jest-tests'),
+            module = id(this.module.split(' ').join('-')),
             testList = create('ol'),
-            testsHeader = create('h3'),
-            moduleHeader;
-        
-        if (!module) {
-            module = create('ol');
-            module.id = this.module.split(' ').join('-');
-            moduleHeader = create('h2');
-            moduleHeader.innerHTML = this.module;
-            module.appendChild(moduleHeader);
-        }
+            testsHeader = create('h3');
         
         testList.id = this.name.split(' ').join('-');
         testsHeader.id = this.name.split(' ').join('-') + '-header';
@@ -38,22 +69,16 @@ Test.prototype = {
     },
     
     run : function () {
-        var self = this;
-        expose(Jest.data, {
-            currentTest : self
-        });
         this.callback();
     },
     
     finish : function () {
-        var module = byId(this.module.split(' ').join('-')),
-            testList = byId(this.name.split(' ').join('-')),
+        var module = id(this.module.split(' ').join('-')),
+            testList = id(this.name.split(' ').join('-')),
             test,
-            moduleHeader,
-            header,
-            i;
+            header;
 
-        header = byId(this.name.split(' ').join('-') + '-header');
+        header = id(this.name.split(' ').join('-') + '-header');
         header.innerHTML = this.name + ' -- ' + (this.actual - this.failed) + ' of ' + this.actual + ' passed';
         
         while (this.results.length) {
@@ -66,12 +91,14 @@ Test.prototype = {
 
 var Jest = {
     jest : function (moduleName) {
-        Jest.data.currentModule = moduleName;
+        var module = new Module(moduleName);
+        Jest.modules.push(module);
+        Jest.currentModule = module;
     },
 
-    test : function (testsName, expectedTests, tests) {
-        var test = new Test(Jest.data.currentModule, testsName, expectedTests, tests);
-        Jest.data.tests.push(test);
+    test : function (testsName, tests, expectedTests) {
+        var test = new Test(Jest.currentModule.name, testsName, expectedTests, tests);
+        Jest.currentModule.tests.push(test);
     },
     
     yes : function (given, message) {
@@ -102,64 +129,68 @@ var Jest = {
     different : function (given, expected, message) {
         var result = !Jest.isEqual(given, expected);
         Jest.process(result, message);
+    },
+    
+    range : function (given, range, message) {
+        var result = ((given > range[0]) && (given < range[1]));
+        Jest.process(result, message);
     }
 };
 
 expose(window, Jest);
 
-var data = {
-    tests : [],
-    jestStats : { total : 0, failed : 0, time : 0 }
-}
-
 expose(Jest, {
-    data : data,
+    jestStats : { total : 0, failed : 0, time : 0 },
+    
+    modules : [], 
     
     load : function () {
-        var i,
-            test,
-            start, 
-            done;
-        start = new Date();
-        
-        while (Jest.data.tests.length) {
-            test = Jest.data.tests.shift();
-            test.setup.call(test);
-            test.run.call(test);
-            test.finish.call(test);
+        var module,
+            done,
+            start = new Date();
+            
+        while (Jest.modules.length) {
+            module = Jest.modules.shift();
+            module.setup.call(module);
+            module.run.call(module);
+            module.finish.call(module);
         }
         
         done = new Date();
-        Jest.data.jestStats.time = done.getTime() - start.getTime();
+        Jest.jestStats.time = done.getTime() - start.getTime();
         Jest.finish();
     },
     
     process : function (result, message) {
-        var resultStr = 'passed',
-            resultData;
+        var resultData,
+            resultStr = 'passed';
+            
         if (!result) {
             resultStr = 'failed';
-            Jest.data.jestStats.failed += 1;
-            Jest.data.currentTest.failed += 1;
+            Jest.jestStats.failed += 1;
+            Jest.currentModule.currentTest.failed += 1;
         }
+        
         resultData = resultStr + ' -- ' + message;
-        Jest.data.jestStats.total += 1;
-        Jest.data.currentTest.actual += 1;
-        Jest.data.currentTest.results.push(resultData);
+        Jest.jestStats.total += 1;
+        Jest.currentModule.currentTest.actual += 1;
+        Jest.currentModule.currentTest.results.push(resultData);
     },
     
     finish : function () {
-        var stat = Jest.data.jestStats;
-            browser = byId('user-info'),
-            passedNumber = byId('passed-number'),
-            light = byId('light'),
-            time = byId('jest-time');
+        var stat = Jest.jestStats,
+            browser = id('user-info'),
+            passedNumber = id('passed-number'),
+            light = id('light'),
+            time = id('jest-time'),
+            ui = userInfo();
         
-        browser.innerHTML = getBrowser();
+        browser.innerHTML = ui.browser + ' ' + ui.version + ' -- ' + ui.os;
         passedNumber.innerHTML += ' ' + (stat.total - stat.failed) + ' of ' + stat.total + ' tests passed &rarr;';
     
-        if (stat.failed) 
+        if (stat.failed) {
             light.style.background = '#FF6464';
+        }
 
         time.innerHTML = stat.time + ' milliseconds';
     },
@@ -170,106 +201,175 @@ expose(Jest, {
             aType = typeof a,
             bType = typeof b;
             
-        if (a === b)    
+        if (a === b) {  
             return true;
+        }
             
-        if (aType !== bType) 
+        if (aType !== bType) {
             return false;
+        }
         
         if (Jest.isArray(a) && Jest.isArray(b)) {
-            if (a.length !== b.length) 
+            if (a.length !== b.length) {
                 return false; 
+            }
                 
             for (i = 0; i < a.length; i+=1) {
-                if (a[i] !== b[i]) 
+                if (a[i] !== b[i]) {
                     return false;
+                }
             }
             
             return true;
         }
         
-        if (aType !== 'object') 
+        if (aType !== 'object') {
             return false;
+        }
             
         for (prop in a) {
-            if (a.hasOwnProperty(prop) && !b.hasOwnProperty(prop)) 
+            if (a.hasOwnProperty(prop) && !b.hasOwnProperty(prop)) {
                 return false;
+            }
                 
-            if (a.hasOwnProperty(prop) && b.hasOwnProperty(prop))
+            if (a.hasOwnProperty(prop) && b.hasOwnProperty(prop)) {
                 return Jest.isEqual(a[prop], b[prop]);
+            }
         }
         
         return true;
     },
     
     isArray :  function (a) {
-        if (a == 'undefined')
+        if (a === 'undefined') {
             return false;
-        else if (a && typeof a === 'object' && a.constructor === Array)
+        } else if (a && typeof a === 'object' && a.constructor === Array) {
             return true;
-        else if (Object.prototype.toString.call(a) == '[object Array]')
+        } else if (Object.prototype.toString.call(a) === '[object Array]') {
             return true;
-        else
+        } else {
             return false;
+        }
     }
-})
+});
 
 addEvent(window, 'load', Jest.load);
 
 function expose (exposed, exposer) {
     for (var property in exposer) {
-        if (exposer.hasOwnProperty(property))
+        if (exposer.hasOwnProperty(property)) {
             exposed[property] = exposer[property]; 
+        }
     }
 }
 
 function addEvent (obj, type, callback) {
-    if (obj.addEventListener)
-        obj.addEventListener(type, callback);
-    else if (obj.attachEvent)
+    if (obj.addEventListener) {
+        obj.addEventListener(type, callback, false);
+    } else if (obj.attachEvent) {
         obj.attachEvent('on' + type, callback);
+    }
 }
 
 function create (elem) {
     return document.createElement(elem);
 }
 
-function byId (id) {
-    var elem = document.getElementById(id);
-    if (elem) 
-        return elem;
-    else 
-        return false;
+function id (str) {
+    return document.getElementById(str);
 }
 
-function getBrowser () {
-    var browser = '',
-        version = navigator.userAgent.split(' '),
-        parseVersion = function (position) {
-            return version[version.length-position].split('/')[1];
-        };
-     
-    if (navigator.vendor === 'Apple Computer, Inc.') {
-        version = parseVersion(2);
-        browser = 'Safari';
-    } else if (navigator.vendor === 'Google Inc.') {
-        version = parseVersion(2);
-        browser = 'Chrome';
-    } else if (window.opera) {
-        version = parseVersion(1);
-        browser = 'Opera';
-    } else if (navigator.appName === "Microsoft Internet Explorer") {
-        version = parseVersion(3);
-        browser = 'Internet Explorer';
-    } else if (navigator.product === 'Gecko') {
-        version = parseVersion(1);
-        browser = 'Firefox';
-    } 
-    return browser + ' ' + version;
-}
+// collects browser, version, & os
+// supports:
+//        Internet Explorer 6, 7, 8, 9
+//        Firefox 3.6, 4, 5, 6, 7
+//        Safari 3, 4, 5
+//        Opera 9, 10, 11, Mini, Mobile 
+//        Chrome
+//
+//        Mac OS X
+//        Windows
+//        Linux
+//        Android
+//        iOS
+function userInfo () {
+    var browser,
+        os,
+        version,
+        string,
+        ua = window.navigator.userAgent,
+        platform = window.navigator.platform,
 
+        info = function () {
+            if ( /MSIE/.test(ua) ) {
+                browser = 'Internet Explorer';
+                if ( /IEMobile/.test(ua) ) {
+                    browser += ' Mobile';
+                }
+                version = /MSIE \d+[.]\d+/.exec(ua)[0].split(' ')[1];  
+            } else if ( /Chrome/.test(ua) ) {
+                browser = 'Chrome';
+                version = /Chrome\/[\d\.]+/.exec(ua)[0].split('/')[1];
+            } else if ( /Opera/.test(ua) ) {
+                browser = 'Opera';
+                if ( /mini/.test(ua) ) {
+                    browser += ' Mini';
+                } else if ( /Mobile/.test(ua) ) {
+                    browser += ' Mobile';
+                } 
+            } else if ( /Android/.test(ua) ) {
+                browser = 'Android Webkit Browser';
+                mobile = true;
+                os = /Android\s[\.\d]+/.exec(ua);
+            } else if ( /Firefox/.test(ua) ) {
+                browser = 'Firefox';
+                if ( /Fennec/.test(ua) ) {
+                    browser += ' Mobile';
+                }
+                version = /Firefox\/[\.\d]+/.exec(ua)[0].split('/')[1];
+            } else if ( /Safari/.test(ua) ) {
+                browser = 'Safari';
+                if ( (/iPhone/.test(ua)) || (/iPad/.test(ua)) || (/iPod/.test(ua)) ) {
+                    os = 'iOS';
+                }
+            }
+
+            if ( !version ) {
+                 version = /Version\/[\.\d]+/.exec(ua);
+                 if (version) {
+                     version = version[0].split('/')[1];
+                 } else {
+                     version = /Opera\/[\.\d]+/.exec(ua)[0].split('/')[1]
+                 }
+            }
+            
+            if ( platform === 'MacIntel' || platform === 'MacPPC' ) {
+                os = 'Mac OS X ' + /10[\.\_\d]+/.exec(ua)[0];
+                if ( /[\_]/.test(os) ) {
+                    os = os.split('_').join('.');
+                }
+            } else if ( platform === 'Win32' ) {
+                os = 'Windows 32 bit';
+            } else if ( platform == 'Win64' ) {
+                os = 'Windows 64 bit';
+            } else if ( !os && /Linux/.test(platform) ) {
+                os = 'Linux';
+            }
+            
+            if ( !os && /Windows/.test(ua) ) {
+                os = 'Windows';
+            }
+            
+        }();
+    return {
+        browser : browser,
+        version : version,
+        os : os
+    };        
+}
 
 }(this));
+
 
 
 
